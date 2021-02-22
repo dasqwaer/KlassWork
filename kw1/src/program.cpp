f39f5435559817b9596295730f2517786c4e8c9d
dasqwaer <dasqwaer@gmail.com>
2021-02-22 20:36:02 +0700

    Files program.cpp,program.h,CMakeLists.txt are full

---
+++ kw1/src/program.cpp
@@ -0,0 +1,7 @@
+#include "program.h"
+using namespace std; // подключение пространства имён стандартной библиотеки, чтобы можно было использовать функции из неё без явного указания
+int main (int argc, char** args)
+{
+cout << "Hello, World!" << endl;
+return SUCCESS;
+}
