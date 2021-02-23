f087e29806bf75baa24842e6d1a35597332b9b4f
dasqwaer <dasqwaer@gmail.com>
2021-02-23 13:49:18 +0700

    create program sizeof

---
+++ kw2/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main (int argc, char**args)
+{
+std::cout << "Привет, мир!" << std::endl;
+
+
+//std::cout << (sizeof('b')) << std::endl;
+std::cout << (sizeof(int)) << std::endl;
+std::cout << (sizeof(char)) << std::endl;
+std::cout << (sizeof(short)) << std::endl;
+std::cout << (sizeof(double)) << std::endl;
+std::cout << (sizeof(long)) << std::endl;
+
+return 0;
+
+}
